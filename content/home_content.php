41d1294fca54544b3cd1cc55d4b53a9eb75aaba8
Jeo <j.cabrigas.writer@gmail.com>
2023-05-02 22:42:20 -0700

    Adds Opening text and 3 food photos for home page

--- content/home_content.php
+++ content/home_content.php
@@ -1,1 +1,53 @@
-<h1 class="text-center">Tenjin Ramen House Restaurant Replica Under Construction</h1>
+<!-- Landing Img -->
+<section id="home-landing-img">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 id="home-welcome">Welcome</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-uppercase">Ramen Lovers</h2>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Opening Text -->
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-uppercase">Tenjin Ramen House Restaurant</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h3>The perfect place to enjoy the life and food.</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <p>A <b>restaurant</b> that specializes in <b>ramen</b> dishes, the wheat-flour noodles in broth. We make different flavors in broth, soup flavors, toppings, noodle texture, and more. Making a delicious ramen isn't easy if you are making the soup from scratch, that's why <b>Tenjin Ramen House</b> is here to offer authentic and delicious Ramen. Visit us at <b>7485 Rush River Dr. #740 Sacramento, California</b></p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Home Food Imgs -->
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <img src="assets/images/home-fried-rice.jpg" />    
+            </div>
+            <div class="col-4">
+                <img src="assets/images/home-salmon.jpg" />
+            </div>
+            <div class="col-4">
+                <img src="assets/images/home-ramen.jpg" />
+            </div>
+        </div>
+    </div>
+</section>
