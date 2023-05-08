0c4664063b766b7e63fc148c9fc3f8beb0e22e60
Jeo <j.cabrigas.writer@gmail.com>
2023-05-07 23:39:46 -0700

    Adds content to contact form section

--- content/about_us_content.php
+++ content/about_us_content.php
@@ -63,7 +63,38 @@
             
             <!-- Questions Form -->
             <div class="col-sm-6">
-
+                <div class="row">
+                    <div class="col-12">
+                        <h4>Questions</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <h4 class="text-uppercase">Let Me Know</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <small>Discaliamer: Filling out this form will send an email to Jamieden Cabrigas and not to Tenjin Ramen House.</small>
+                    </div>
+                </div>
+                <form>
+                    <div class="row">
+                        <div class="col-12">
+                            <input type="text" name="full_name" placerholder="Full Name" />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <input type="email" name="email" placerholder="Email" />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <textarea type="text" name="message" placerholder="Message"></textarea>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
