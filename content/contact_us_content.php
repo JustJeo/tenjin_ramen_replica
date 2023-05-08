c5194579191eb5be37cfbbb8501a6c09fe819625
Jeo <j.cabrigas.writer@gmail.com>
2023-05-07 23:11:42 -0700

    Sets up contact us page layout

---
+++ content/contact_us_content.php
@@ -0,0 +1,94 @@
+<!-- Landing Img -->
+<section id="home-landing-img">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-12">
+                <h2 id="home-welcome">Welcome</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-uppercase">Ramen Lovers</h2>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Opening Text -->
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-uppercase">Get In Touch</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h3>Visit us today.</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <p>Friendly staff and good food are waiting for you. Come by at our restaurant today.</p>
+            </div>
+            <div class="col-lg-6">
+                <img src="assets/images/welcome-sign.jpg" style="width: 100%;"/>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Hours & Questions -->
+<section>
+    <div class="container">
+        <div class="row">
+            <!-- Hours of Operation -->
+            <div class="col-6">
+
+            </div>
+            
+            <!-- Questions Form -->
+            <div class="col-6">
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Google Maps & Ways to Contact -->
+<section>
+    <div class="container">
+        <div class="row">
+            <!-- Google Maps -->
+            <div class="col-lg-8">
+
+            </div>
+
+            <!-- Address & Contact -->
+            <div class="col-lg-4">
+                <!-- Address -->
+                <div class="row">
+                    <div class="col-12">
+                        <h3 class="text-uppercase">Address</h3>
+                    </div>
+                    <div class="col-12">
+                        <h4>7485 Rush River Dr. 740</h4>
+                        <h4>Sacramento CA 95831</h4>
+                    </div>
+                </div>
+                <!-- Contact -->
+                <div class="row">
+                    <div class="col-12">
+                        <h3 class="text-uppdercase">Contact</h3>
+                    </div>
+                    <div class="col-12">
+                        <h4>tenjinramen@yahoo.com</h4>
+                    </div>
+                    <div class="col-12">
+                        <h4>(916) 661-6656</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
