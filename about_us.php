b171e9d09c9dcd4ab645bdf18f190c638c32c9b5
Jeo <j.cabrigas.writer@gmail.com>
2023-05-07 23:06:57 -0700

    Sets up about us page

---
+++ about_us.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "sections/html_head.php" ;?>
+</head>
+<body>
+    <!-- NavBar -->
+    <?php include "sections/navbar.php" ;?>
+
+    <!-- Content -->
+    <?php include "content/about_us_content.php" ;?>
+
+    <!-- Footer -->
+    <?php include "sections/footer.php" ;?>
+
+    <!-- End Scripts -->
+    <?php include "sections/js_end_scripts.php" ;?>
+</body>
+</html>
---
+++ content/about_us_content.php
@@ -0,0 +1,91 @@
+<!-- Landing Img -->
+<section id="home-landing-img">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-12">
+                <h2 id="home-welcome">Welcome</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-uppercase">Ramen Lovers</h2>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Opening Text -->
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-uppercase">Few Words About Us</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h3>Serving best Ramen since 2018</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <p>It resonates very nice with the fact of how universally cozy and welcoming our atmosphere is. All our customers say that when they eat at <b>Tenjin Ramen House,</b> they feel like having a nice dinner at home. These are sweet words, which just melt our hearts with joy!</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Hours & Questions -->
+<section>
+    <div class="container">
+        <div class="row">
+            <!-- Hours of Operation -->
+            <div class="col-6">
+
+            </div>
+            
+            <!-- Questions Form -->
+            <div class="col-6">
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Google Maps & Ways to Contact -->
+<section>
+    <div class="container">
+        <div class="row">
+            <!-- Google Maps -->
+            <div class="col-lg-8">
+
+            </div>
+
+            <!-- Address & Contact -->
+            <div class="col-lg-4">
+                <!-- Address -->
+                <div class="row">
+                    <div class="col-12">
+                        <h3 class="text-uppercase">Address</h3>
+                    </div>
+                    <div class="col-12">
+                        <h4>7485 Rush River Dr. 740</h4>
+                        <h4>Sacramento CA 95831</h4>
+                    </div>
+                </div>
+                <!-- Contact -->
+                <div class="row">
+                    <div class="col-12">
+                        <h3 class="text-uppdercase">Contact</h3>
+                    </div>
+                    <div class="col-12">
+                        <h4>tenjinramen@yahoo.com</h4>
+                    </div>
+                    <div class="col-12">
+                        <h4>(916) 661-6656</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
