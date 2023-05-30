3c2deaa6a478637e15633987a2f9b5b6706175c0
Jeo <j.cabrigas.writer@gmail.com>
2023-05-30 00:25:19 -0700

    Adds styling to navbar

--- sections/navbar.php
+++ sections/navbar.php
@@ -1,17 +1,46 @@
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light nav_bg">
+    <nav class="navbar navbar-expand-lg nav_bg">
 		<div class="container">
             <a class="navbar-brand" href="#">
-				<img src="assets/images/logo.png" alt="Tenjin Ramen House Logo" >
+                <img src="assets/images/logo.png" alt="Tenjin Ramen House Logo" >
 			</a>
-            <ul class="text-uppercase">
-                <li>Home</li>
-                <li>About Us</li>
-                <li>E-Giftcard Balance</li>
-                <li>Contact Us</li>
-            </ul>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav align-items-center text-uppercase">
+                    <li class="nav-item">
+						<a class="nav-link" href="/home.php">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/about_us.php">About Us</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">E-Giftcard Balance</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/contact_us.php">Contact Us</a>
+					</li>
+                </ul>
+            </div>
 
-            <button class="btn btn-primary text-uppercase">Order Online</button>
+            <button class="btn btn-primary text-uppercase" id="order-online-btn">Order Online</button>
         </div>
     </nav>
+
+    <!-- Landing Img -->
+    <section id="nav-landing-img">
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-12">
+                    <h2 id="nav-welcome">Welcome</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <h2 class="text-uppercase">Ramen Lovers</h2>
+                </div>
+            </div>
+        </div>
+    </section>
 </header>
