5f1e0268c774627ffae6d15463aa0e8c96c4311b
Jeo <j.cabrigas.writer@gmail.com>
2023-05-30 00:12:22 -0700

    Adds styling and logo to navbar

--- sections/navbar.php
+++ sections/navbar.php
@@ -1,13 +1,17 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light nav_bg">
 		<div class="container">
-        <ul class="text-uppercase">
-            <li>Home</li>
-            <li>About Us</li>
-            <li>E-Giftcard Balance</li>
-            <li>Contact Us</li>
-        </ul>
+            <a class="navbar-brand" href="#">
+				<img src="assets/images/logo.png" alt="Tenjin Ramen House Logo" >
+			</a>
+            <ul class="text-uppercase">
+                <li>Home</li>
+                <li>About Us</li>
+                <li>E-Giftcard Balance</li>
+                <li>Contact Us</li>
+            </ul>
 
-        <button class="btn btn-primary text-uppercase">Order Online</button>
+            <button class="btn btn-primary text-uppercase">Order Online</button>
+        </div>
     </nav>
 </header>
