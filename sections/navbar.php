55bb529b0a5723b086e131dacbbe996ad0b73a73
Jeo <j.cabrigas.writer@gmail.com>
2023-05-30 00:05:10 -0700

    Adds a navbar

---
+++ sections/navbar.php
@@ -0,0 +1,13 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light nav_bg">
+		<div class="container">
+        <ul class="text-uppercase">
+            <li>Home</li>
+            <li>About Us</li>
+            <li>E-Giftcard Balance</li>
+            <li>Contact Us</li>
+        </ul>
+
+        <button class="btn btn-primary text-uppercase">Order Online</button>
+    </nav>
+</header>
