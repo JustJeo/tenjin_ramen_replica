5af7ed5dcb70a8f376c608e0a4bc68b5405b3502
Jeo <j.cabrigas.writer@gmail.com>
2023-06-12 23:51:38 -0700

    Adds cards classes to hours and questions cta section

--- sections/hours_and_questions.php
+++ sections/hours_and_questions.php
@@ -3,57 +3,65 @@
         <div class="row">
             <!-- Hours of Operation -->
             <div class="col-sm-6">
-                <div class="row">
-                    <div class="col-12">
-                        <h4>Hours of</h4>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <h4 class="text-uppercase">Operation</h4>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <p class="text-uppercase">WEDNESDAY: 4:00 PM - 8:00 PM THURSDAY, FRIDAY , SATURDAY, SUNDAY 11:00 AM - 3:00 PM | 4:00 PM - 8:30 PM MONDAY & TUESDAY: CLOSED</p>
+                <div class="card" id="hours-cta-card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-12">
+                                <h4>Hours of</h4>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <h4 class="text-uppercase">Operation</h4>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <p class="text-uppercase">WEDNESDAY: 4:00 PM - 8:00 PM THURSDAY, FRIDAY , SATURDAY, SUNDAY 11:00 AM - 3:00 PM | 4:00 PM - 8:30 PM MONDAY & TUESDAY: CLOSED</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
             
             <!-- Questions Form -->
             <div class="col-sm-6">
-                <div class="row">
-                    <div class="col-12">
-                        <h4>Questions</h4>
+                <div class="card" id="questions-cta-card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-12">
+                                <h4>Questions</h4>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <h4 class="text-uppercase">Let Me Know</h4>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <small>Discaliamer: Filling out this form will send an email to Jamieden Cabrigas and not to Tenjin Ramen House.</small>
+                            </div>
+                        </div>
+                        <form>
+                            <div class="row">
+                                <div class="col-12">
+                                    <input type="text" name="full_name" placerholder="Full Name" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <input type="email" name="email" placerholder="Email" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <textarea type="text" name="message" placerholder="Message"></textarea>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-12">
-                        <h4 class="text-uppercase">Let Me Know</h4>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <small>Discaliamer: Filling out this form will send an email to Jamieden Cabrigas and not to Tenjin Ramen House.</small>
-                    </div>
-                </div>
-                <form>
-                    <div class="row">
-                        <div class="col-12">
-                            <input type="text" name="full_name" placerholder="Full Name" />
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <input type="email" name="email" placerholder="Email" />
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <textarea type="text" name="message" placerholder="Message"></textarea>
-                        </div>
-                    </div>
-                </form>
             </div>
         </div>
     </div>
