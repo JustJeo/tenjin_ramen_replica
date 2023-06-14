f9e82b58dead4d67ec81c757d8eb77b19f976127
Jeo <j.cabrigas.writer@gmail.com>
2023-06-13 22:28:21 -0700

    Gets rid of x-index scroll

--- sections/footer.php
+++ sections/footer.php
@@ -1,16 +1,18 @@
 <footer>
     <div class="footer">
-        <div class="row">
-            <div class="col-lg-8">
-                <!-- Copyright -->
-                <div class="copyright-content">
-                    <div class="container">
-                        <div class="copyright-text text-center ">Copyright &copy; <?php echo date("Y");?> Tenjing Ramen House Replica. All rights reserved.</div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <!-- Copyright -->
+                    <div class="copyright-content">
+                        <div class="container">
+                            <div class="copyright-text text-center ">Copyright &copy; <?php echo date("Y");?> Tenjing Ramen House Replica. All rights reserved.</div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-4">
-                <p>Replicated by <a href="https://www.justjeo.com/">Just Jeo</a></p>
+                <div class="col-lg-4">
+                    <p>Replicated by <a href="https://www.justjeo.com/">Just Jeo</a></p>
+                </div>
             </div>
         </div>
     </div>
